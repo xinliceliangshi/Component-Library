dadbb69057ef260b3f76c481287a73b2afd49187
wangying <2772567171@qq.com>
2023-06-12 11:12:34 +0800

    ADD|文档建设更新

--- doc/.vitepress/theme/index.ts
+++
@@ -1,9 +0,0 @@
-import Theme from 'vitepress/dist/client/theme-default'
-import SmartyUI from '../../../src/entry'
-
-export default {
-  ...Theme,
-  enhanceApp({ app }) {
-    app.use(SmartyUI)
-  },
-}
